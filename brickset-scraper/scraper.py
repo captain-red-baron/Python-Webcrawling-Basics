1ba8be1509cf457c3330d5def3c037ac36bd2a69
Marcel Müller <marcel.mueller@siemens.com>
2017-07-17 19:47:34 +0200

    Adding basic crawling functionality for one web page.

--- brickset-scraper/scraper.py
+++ brickset-scraper/scraper.py
@@ -5,5 +5,17 @@ class BrickSetSpider(scrapy.Spider):
     name = "brickset_spider"
     start_urls = ['http://brickset.com/sets/year-2016']
 
-
+    def parse(self, response):
+        set_selector = '.set'
+        for brickset in response.css(set_selector):
+            name_selector = 'h1 a::text'
+            pieces_selector = './/dl[dt/text() = "Pieces"]/dd/a/text()'
+            minifigs_selector = './/dl[dt/text() = "Minifigs"]/dd[2]/a/text()'
+            image_selector = 'img ::attr(src)'
+            yield {
+                'name': brickset.css(name_selector).extract_first(),
+                'pieces': brickset.xpath(pieces_selector).extract_first(),
+                'minifigs': brickset.xpath(minifigs_selector).extract_first(),
+                'image': brickset.css(image_selector).extract_first(),
+            }
 
